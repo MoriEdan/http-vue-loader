17ba5e9b5b46461c121038b46db2a30ad60da716
Ivanq <imachug@yandex.ru>
2018-05-09 16:03:52 +0300

    Use fetch(), not XHR

--- src/httpVueLoader.js
+++ src/httpVueLoader.js
@@ -433,25 +433,8 @@
 	};
 
 	httpVueLoader.httpRequest = function(url) {
-
-		return new Promise(function(resolve, reject) {
-
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url);
-
-			xhr.onreadystatechange = function() {
-
-				if ( xhr.readyState === 4 ) {
-
-					if ( xhr.status >= 200 && xhr.status < 300 )
-						resolve(xhr.responseText);
-					else
-						reject(xhr.status);
-				}
-			};
-
-			xhr.send(null);
-		});
+		return fetch(url)
+			.then(res => res.text());
 	};
 
 	httpVueLoader.langProcessor = {
